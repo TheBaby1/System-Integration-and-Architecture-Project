4a8ddd1028700fdbf1cf0a82386181a47078979b
Nikko <nikkonuriel@gmail.com>
2024-11-30 03:09:35 +0800

    added/changed minor things

--- crm-service/server.js
+++ crm-service/server.js
@@ -6,23 +6,31 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get('/api/customers', (res, req) => {
-    res.status(200).json([
-        { id: 1, name: 'Nichole Alburo', email: 'vine@gmail.com' },
-        { id: 2, name: 'Sareljohn Pebida', email: 'john@hotmail.com' },
-        { id: 3, name: 'Nikko Ensomo', email: 'nikko@yahoo.com' }
-    ])
+app.get('/api/customers', (req, res) => {
+
+    try {
+        res.status(200).json([
+            { id: 1, name: 'Nichole Alburo', email: 'vine@gmail.com' },
+            { id: 2, name: 'Sareljohn Pebida', email: 'john@hotmail.com' },
+            { id: 3, name: 'Nikko Ensomo', email: 'nikko@yahoo.com' }
+        ])
+        res.status(200).json(customers);
+    } catch (error) {
+        console.log('Failed to fetch customers:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+    
 })
 
 
 app.post('/api/customers', (req, res) => {
 
     try {
-        const { name, enail } = req.body;
+        const { name, email } = req.body;
         const newCustomer = { id: Date.now(), name, email };
         res.status(201).json(newCustomer);
     } catch (error) {
-        console.log('Failed to get cusomers:', error);
+        console.log('Failed to create customers:', error);
         res.status(500).json( 'Internal Server Error' );
     }
 })
